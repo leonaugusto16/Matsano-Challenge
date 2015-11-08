6da7bddd74c3f86ff741655ae0c82f0e2259fa3f
Leon Augusto <leonaugusto16@gmail.com>
2015-11-08 16:27:35 -0200

    finished challenge 3

--- set13.py
+++ set13.py
@@ -1,67 +1,41 @@
-#Single-byte XOR cipher
+import binascii,sys
+#list words in english http://www.englishexperts.com.br/forum/1000-palavras-mais-usadas-em-ingles-t4469.html
+keywords=['the', 'of', 'to', 'and', 'a', 'in', 'is', 'it', 'you', 'that', 'he', 'was', 'for', 'on', 'are', 'with', 'as', 'I', 'his', 'they', 'be', 'at', 'one', 'have', 'this', 'from', 'or', 'had', 'by', 'hot', 'word', 'but', 'what', 'some', 'we', 'can', 'out', 'other', 'were', 'all', 'there', 'when', 'up', 'use', 'your', 'how', 'said', 'an', 'each', 'she', 'wich', 'do', 'their', 'time', 'if', 'will', 'way', 'about', 'many', 'then', 'them', 'write', 'would', 'like', 'so', 'these', 'her', 'long', 'make', 'thing', 'see', 'him', 'two', 'has', 'look', 'more', 'day', 'could', 'go', 'come', 'did', 'number', 'sound', 'no', 'most', 'people', 'my', 'over', 'know', 'water', 'than', 'call', 'first', 'who', 'may', 'down', 'side', 'been', 'now', 'find']
 
-#from collections import Counter
-import sys
-import crypto
-#import textutils
-#import profile
-from collections import defaultdict
-from operator import itemgetter, attrgetter, methodcaller
-import binascii
+def decrypt(encoded,key):
+    xor=lambda x ,y :int(x,16)^int(y,16)
+    ans=""
+    for temp in [encoded[a:a+2] for a in range(0,len(encoded)-1,2)]:
+        ans=ans+chr(xor(temp,key))
+    return ans
+       
 
-prob = [
-    ('a', 0.0651738),
-    ('b', 0.0124248),
-    ('c', 0.0217339),
-    ('d', 0.0349835),
-    ('e', 0.1041442),
-    ('f', 0.0197881),
-    ('g', 0.0158610),
-    ('h', 0.0492888),
-    ('i', 0.0558094),
-    ('j', 0.0009033),
-    ('k', 0.0050529),
-    ('l', 0.0331490),
-    ('m', 0.0202124),
-    ('n', 0.0564513),
-    ('o', 0.0596302),
-    ('p', 0.0137645),
-    ('q', 0.0008606),
-    ('r', 0.0497563),
-    ('s', 0.0515760),
-    ('t', 0.0729357),
-    ('u', 0.0225134),
-    ('v', 0.0082903),
-    ('w', 0.0171272),
-    ('x', 0.0013692),
-    ('y', 0.0145984),
-    ('z', 0.0007836),
-    (' ', 0.1918182)
-]
+def brute_xor(encoded):
+   
+    hex_key="0"
 
-hexa = "1b37373331363f78151b7f2b783431333d78397828372d363c78373e783a393b3736"
-string = binascii.unhexlify(hexa)
-
-def letters_freqs(string, top=100):
-    counts = defaultdict(int)
-    for x in string:
-        counts[x] += 1
-    return sorted(counts.items(), reverse=True, key=lambda tup: tup[1])[:top]
-
-
-def replace_string(string):
-	prob_order = sorted(prob, key=itemgetter(1), reverse=True)
-	freqs = letters_freqs(string)
+    #100 most frequently used words in the English, we scan the text looking for them, this indicates a high probability the correct phrase
+    for a in range(0,255):
+        ans=decrypt(encoded,hex_key)
+        for word in keywords:
+	    word = " "+word+" "
+            if word in ans or word+" " in ans or " "+word in ans or " "+word+" " in ans:
+		print "[*]Decrypted data : "+ans
+                print "[*]key : 0x"+(hex_key)
+		print "[*]Word Denounced : "+word
+		break
 	
-	i=0	
-	for x in freqs:
-		string= string.replace(x[0][0],prob_order[i][0])
-		i=i+1
-	return string
+	#Increment key hex
+	int_key=int(hex_key,16)
+    	int_key=int_key+1
+    	hex_key=hex(int_key)[2:]
 
 
 if __name__ == '__main__':
-	encodedS = '1b37373331363f78151b7f2b783431333d78397828372d363c78373e783a393b3736'
-	s = binascii.unhexlify(encodedS)
 	
-	#print replace_string(encodedS)
+	encoded = '1b37373331363f78151b7f2b783431333d78397828372d363c78373e783a393b3736'
+	print "[*]Encrypted data : "+encoded
+	print "\n[+]Starting Decrpytion ...\n"
+        brute_xor(encoded)
+
+        print "\n[+] Done\n"
